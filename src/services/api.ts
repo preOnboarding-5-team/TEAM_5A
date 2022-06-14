e6681f556afcc35f1556d3b2f2954895ef0f3694
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-15 03:34:19 +0900

    fix: Change server

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,24 +3,27 @@ import axios from 'axios';
 
 const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 
-const SEARCH_DISEASES_BASE_URL = `${PROXY}/B551182/diseaseInfoService/getDissNameCodeList`;
+// const SEARCH_DISEASES_BASE_URL = `${PROXY}/B551182/diseaseInfoService/getDissNameCodeList`;
+const SEARCH_DISEASES_BASE_URL = 'ec2-18-207-186-148.compute-1.amazonaws.com:8080';
 
 interface Params {
   searchText: string;
 }
 
 export const getSearchDiseasesAPI = async (params: Params) => {
-  const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
-    params: {
-      ServiceKey: process.env.REACT_APP_GET_DISS_API_KEY,
-      ...params,
-      _type: 'json',
-      numOfRows: 2000,
-      sickType: 1,
-      medTp: 2,
-      diseaseType: 'SICK_NM',
-    },
-  });
+  // const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
+  //   params: {
+  //     ServiceKey: process.env.REACT_APP_GET_DISS_API_KEY,
+  //     ...params,
+  //     _type: 'json',
+  //     numOfRows: 2000,
+  //     sickType: 1,
+  //     medTp: 2,
+  //     diseaseType: 'SICK_NM',
+  //   },
+  // });
+
+  const { data } = await axios.get<IDiseaseAPIRes>(SEARCH_DISEASES_BASE_URL);
 
   const item = data.response.body?.items?.item;
   if (item === undefined) return;
@@ -29,16 +32,18 @@ export const getSearchDiseasesAPI = async (params: Params) => {
 };
 
 export const getAllDiseasesApi = async () => {
-  const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
-    params: {
-      _type: 'json',
-      ServiceKey: process.env.REACT_APP_GET_DISS_API_KEY,
-      numOfRows: 2000,
-      sickType: 1,
-      medTp: 2,
-      diseaseType: 'SICK_NM',
-    },
-  });
+  // const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
+  //   params: {
+  //     _type: 'json',
+  //     ServiceKey: process.env.REACT_APP_GET_DISS_API_KEY,
+  //     numOfRows: 2000,
+  //     sickType: 1,
+  //     medTp: 2,
+  //     diseaseType: 'SICK_NM',
+  //   },
+  // });
+
+  const { data } = await axios.get<IDiseaseAPIRes>(SEARCH_DISEASES_BASE_URL);
 
   const item = data.response.body?.items?.item;
   if (item === undefined) return;
