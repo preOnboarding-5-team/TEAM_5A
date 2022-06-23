2c38690faa6834db39cc52b83d6df0983049a2d0
easeurmind <64529155+restareaByWeezy@users.noreply.github.com>
2022-06-23 20:51:02 +0900

    Update api.ts
    fix: api server connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 /* eslint-disable consistent-return */
 import axios from 'axios';
 
-// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
-
-// const SEARCH_DISEASES_BASE_URL = `${PROXY}/B551182/diseaseInfoService/getDissNameCodeList`;
 const SEARCH_DISEASES_BASE_URL = 'https://human-server.click/';
 
 interface Params {
@@ -11,22 +8,14 @@ interface Params {
 }
 
 export const getSearchDiseasesAPI = async (params: Params) => {
-  // const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
-  //   params: {
-  //     ServiceKey: process.env.REACT_APP_GET_DISS_API_KEY,
-  //     ...params,
-  //     _type: 'json',
-  //     numOfRows: 2000,
-  //     sickType: 1,
-  //     medTp: 2,
-  //     diseaseType: 'SICK_NM',
-  //   },
-  // });
+  const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
+    params: {
+      ...params,
+      _type: 'json',
+    },
+  });
 
-  const { data } = await axios.get<IBody>(SEARCH_DISEASES_BASE_URL);
-
-  // const item = data.response.body?.items?.item;
-  const item = data.items?.item;
+  const item = data.response.body?.items?.item;
   if (item === undefined) return;
   if (item instanceof Array) return item;
   return [item];
@@ -36,22 +25,12 @@ export const getAllDiseasesApi = async () => {
   const { data } = await axios.get<IDiseaseAPIRes>(`${SEARCH_DISEASES_BASE_URL}`, {
     params: {
       _type: 'json',
-//       ServiceKey: process.env.REACT_APP_GET_DISS_API_KEY,
       numOfRows: 2000,
-      sickType: 1,
-      medTp: 2,
-      diseaseType: 'SICK_NM',
+      ServiceKey: process.env.REACT_APP_DISEASES_ID,
     },
   });
 
-  const res = await axios.get<IBody>(SEARCH_DISEASES_BASE_URL);
-
-  console.log(res);
-
-  const { data } = res;
-
-  // const item = data.response.body?.items?.item;
-  const item = data.items?.item;
+  const item = data.response.body?.items?.item;
   if (item === undefined) return;
   if (item instanceof Array) return item;
   return [item];
